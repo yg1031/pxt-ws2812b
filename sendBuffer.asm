0878e9bd7152365d9572ce5244bfda93078056a2
yg1031 <813090046@qq.com>
2019-06-25 09:47:14 +0800

    v1.1

--- sendBuffer.asm
+++ sendBuffer.asm
@@ -227,50 +227,7 @@ sendBufferAsm:
     nop    
     tst r6, r0          ;            C3
     bne .islate         ;            C4
-    str r1, [r2, #0]    ; pin := lo  C6      
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop    
+    str r1, [r2, #0]    ; pin := lo  C6 
 .islate:
     lsrs r6, r6, #1     ; r6 >>= 1   C7
     bne .justbit        ;            C8
