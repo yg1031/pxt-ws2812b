c46c2c0429875898bbeffbbaf34467669465e1e7
yg1031 <813090046@qq.com>
2019-07-01 09:14:06 +0800

    v1.4.2

--- sendBuffer.asm
+++ sendBuffer.asm
@@ -38,7 +38,7 @@ sendBufferAsm:
 
 .delaya:
     adds r7, #1
-    cmp r7, #30
+    cmp r7, #10         ; 2.4us
     bne .delaya
     b .goa
 
@@ -76,7 +76,7 @@ sendBufferAsm:
 
 .delayc:
     adds r7, #1
-    cmp r7, #30
+    cmp r7, #10
     bne .delayc
     b .goc
 
